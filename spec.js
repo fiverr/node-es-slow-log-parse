29757d6c6d8aa1c6bf8055dfa8288d9ebe563870
Omri Lotan <516342+omrilotan@users.noreply.github.com>
2019-09-10 18:07:02 +0300

    Add tests to time format

--- spec.js
+++ spec.js
@@ -5,7 +5,11 @@ describe('ES', () => {
     fixtures.forEach(
         ([entry, result], index) => {
             test(`Should parse entry ${index} correctly`, () => {
-                expect(es(entry)).toEqual(result);
+                const parsed = es(entry);
+                
+                expect(parsed, 'match results').toEqual(result);
+                expect(parsed.timestamp, 'timestamp format').to.be.a('number');
+                expect(`${parsed.timestamp}`, 'timestamp format').to.to.have.lengthOf(13);
             });
         }
     );
